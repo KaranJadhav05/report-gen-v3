3fd0dd0b7f9fabe7db8f7811253b3f3b193bd98e
Karan Jadhav <karansjadhav7@gmail.com>
2026-03-23 22:36:30 +0530

    Change Proxy URL

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,14 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: { alias: { '@': path.resolve(__dirname, './src') } },
+  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
   server: {
     port: 5173,
-    proxy: { '/api': { target: 'http://localhost:5001', changeOrigin: true } }
-  }
+    proxy: {
+      "/api": {
+        target: "https://report-gen-v3.onrender.com",
+        changeOrigin: true,
+      },
+    },
+  },
 });
